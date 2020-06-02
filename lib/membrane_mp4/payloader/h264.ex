6f8f64f5bb93f131e318a4904437d23a4a0bb271
Mateusz Front <mateusz.front@swmansion.com>
2020-06-02 10:11:32 +0200

    adjust to updated h264 parser

--- lib/membrane_mp4/payloader/h264.ex
+++ lib/membrane_mp4/payloader/h264.ex
@@ -1,6 +1,10 @@
 defmodule Membrane.MP4.Payloader.H264 do
   @moduledoc """
   Payloads H264 stream so it can be embedded in MP4.
+
+  This element requires `h264.nalus` metadata entry to be present in each buffer.
+  This can be achieved by setting `attach_nalus?` option to `true` in the h264
+  parser.
   """
   use Bunch
   use Membrane.Filter
@@ -12,13 +16,13 @@ defmodule Membrane.MP4.Payloader.H264 do
 
   def_input_pad :input,
     demand_unit: :buffers,
-    caps: {Membrane.Caps.Video.H264, stream_format: :byte_stream, alignment: :nal}
+    caps: {Membrane.Caps.Video.H264, stream_format: :byte_stream, alignment: :au}
 
   def_output_pad :output, caps: Membrane.MP4.Payload
 
   @impl true
   def handle_init(_) do
-    {:ok, %{access_unit: [], metadata: nil}}
+    {:ok, %{}}
   end
 
   @impl true
@@ -27,18 +31,21 @@ defmodule Membrane.MP4.Payloader.H264 do
   end
 
   @impl true
-  def handle_process(:input, %Buffer{} = buffer, ctx, state) do
-    {actions, state} = maybe_send_access_unit(buffer.metadata, ctx, state)
-    state = state |> Map.update!(:access_unit, &[buffer | &1])
-    {{:ok, actions ++ [redemand: :output]}, state}
-  end
+  def handle_process(:input, buffer, ctx, state) do
+    %Buffer{payload: payload, metadata: metadata} = buffer
+    {nalus, metadata} = process_metadata(metadata)
+    nalus = Enum.map(nalus, &Map.put(&1, :payload, :binary.part(payload, &1.unprefixed_poslen)))
 
-  @impl true
-  def handle_end_of_stream(:input, ctx, state) do
-    {actions, state} =
-      maybe_send_access_unit(%{new_access_unit: true, access_unit: nil}, ctx, state)
+    caps =
+      if ctx.pads.output.caps do
+        []
+      else
+        [caps: {:output, generate_caps(ctx.pads.input.caps, nalus)}]
+      end
 
-    {{:ok, actions ++ [end_of_stream: :output]}, state}
+    payload = nalus |> Enum.map(&process_nalu/1) |> Enum.join()
+    buffer = %Buffer{payload: payload, metadata: metadata}
+    {{:ok, caps ++ [buffer: {:output, buffer}, redemand: :output]}, state}
   end
 
   @impl true
@@ -46,85 +53,16 @@ defmodule Membrane.MP4.Payloader.H264 do
     {:ok, state}
   end
 
-  defp maybe_send_access_unit(%{new_access_unit?: false}, _ctx, state) do
-    {[], state}
-  end
-
-  defp maybe_send_access_unit(metadata, _ctx, %{access_unit: []} = state) do
-    {[], %{state | metadata: metadata}}
-  end
-
-  defp maybe_send_access_unit(new_metadata, ctx, state) do
-    access_unit = state.access_unit |> Enum.reverse()
-
-    caps =
-      if ctx.pads.output.caps do
-        []
-      else
-        [caps: {:output, generate_caps(ctx.pads.input.caps, access_unit)}]
-      end
-
-    payload = access_unit |> Enum.map(&process_nalu/1) |> Enum.join()
-
-    buffer =
-      %Buffer{payload: payload, metadata: generate_access_unit_metadata(state.metadata)}
-      ~> [buffer: {:output, &1}]
-
-    {caps ++ buffer, %{state | access_unit: [], metadata: new_metadata}}
-  end
-
-  defp process_nalu(%Buffer{metadata: %{type: type}})
-       when type in [:aud, :sps, :pps] do
+  defp process_nalu(%{metadata: %{h264: %{type: type}}}) when type in [:aud, :sps, :pps] do
     <<>>
   end
 
-  defp process_nalu(buffer) do
-    nalu = unprefix(buffer.payload)
-    <<byte_size(nalu)::integer-size(@nalu_length_size)-unit(8), nalu::binary>>
+  defp process_nalu(%{payload: payload}) do
+    <<byte_size(payload)::integer-size(@nalu_length_size)-unit(8), payload::binary>>
   end
 
-  defp unprefix(<<0, 0, 0, 1, nalu::binary>>), do: nalu
-  defp unprefix(<<0, 0, 1, nalu::binary>>), do: nalu
-
-  defp generate_caps(input_caps, access_unit) do
-    avcc = generate_avcc(access_unit)
-    {timescale, sample_duration} = input_caps.framerate
-
-    %Membrane.MP4.Payload{
-      timescale: timescale * 1024,
-      sample_duration: sample_duration * 1024,
-      width: input_caps.width,
-      height: input_caps.height,
-      content: %AVC1{avcc: avcc}
-    }
-  end
-
-  defp generate_avcc(access_unit) do
-    %{sps: sps, pps: pps} =
-      access_unit
-      |> Enum.group_by(
-        fn
-          %Buffer{metadata: %{type: type}} when type in [:sps, :pps] -> type
-          _ -> nil
-        end,
-        &unprefix(&1.payload)
-      )
-
-    <<_idc_and_type, profile, compatibility, level, _::binary>> = hd(sps)
-
-    <<1, profile, compatibility, level, 0b111111::6, @nalu_length_size - 1::2-integer, 0b111::3,
-      length(sps)::5-integer, encode_parameter_sets(sps)::binary, length(pps)::8-integer,
-      encode_parameter_sets(pps)::binary>>
-  end
-
-  defp encode_parameter_sets(pss) do
-    pss
-    |> Enum.map(&<<byte_size(&1)::16-integer, &1::binary>>)
-    |> Enum.join()
-  end
-
-  defp generate_access_unit_metadata(metadata) do
-    {%{key_frame?: key_frame?} = access_unit, metadata} = Map.pop(metadata, :access_unit)
+  defp process_metadata(metadata) do
+    %{h264: %{key_frame?: key_frame?}} = metadata
 
     is_leading = 0
     depends_on = if key_frame?, do: 2, else: 1
@@ -138,6 +76,35 @@ defmodule Membrane.MP4.Payloader.H264 do
       <<0::4, is_leading::2, depends_on::2, is_depended_on::2, has_redundancy::2,
         padding_value::3, non_sync::1, degradation_priority::16>>
 
-    metadata |> Map.merge(access_unit) |> Map.merge(%{mp4_sample_flags: flags})
+    metadata
+    |> Map.merge(%{mp4_sample_flags: flags, key_frame?: key_frame?})
+    |> pop_in([:h264, :nalus])
+  end
+
+  defp generate_caps(input_caps, nalus) do
+    {timescale, sample_duration} = input_caps.framerate
+
+    %Membrane.MP4.Payload{
+      timescale: timescale * 1024,
+      sample_duration: sample_duration * 1024,
+      width: input_caps.width,
+      height: input_caps.height,
+      content: %AVC1{avcc: generate_avcc(nalus)}
+    }
+  end
+
+  defp generate_avcc(nalus) do
+    %{sps: sps, pps: pps} = Enum.group_by(nalus, & &1.metadata.h264.type, & &1.payload)
+    <<_idc_and_type, profile, compatibility, level, _::binary>> = hd(sps)
+
+    <<1, profile, compatibility, level, 0b111111::6, @nalu_length_size - 1::2-integer, 0b111::3,
+      length(sps)::5-integer, encode_parameter_sets(sps)::binary, length(pps)::8-integer,
+      encode_parameter_sets(pps)::binary>>
+  end
+
+  defp encode_parameter_sets(pss) do
+    pss
+    |> Enum.map(&<<byte_size(&1)::16-integer, &1::binary>>)
+    |> Enum.join()
   end
 end
--- test/membrane_mp4/integration_test.exs
+++ test/membrane_mp4/integration_test.exs
@@ -4,36 +4,10 @@ defmodule Membrane.MP4.IntegrationTest do
   alias Membrane.MP4.Container
   alias Membrane.Testing
 
-  defmodule Timestamper do
-    use Membrane.Filter
-
-    def_input_pad :input, caps: :any, demand_unit: :buffers
-    def_output_pad :output, caps: :any
-
-    @impl true
-    def handle_init(_) do
-      {:ok, 0}
-    end
-
-    @impl true
-    def handle_demand(:output, size, :buffers, _ctx, state) do
-      {{:ok, demand: {:input, size}}, state}
-    end
-
-    @impl true
-    def handle_process(:input, buffer, ctx, timestamp) do
-      buffer = Bunch.Struct.put_in(buffer, [:metadata, :timestamp], trunc(timestamp))
-      {num, denom} = ctx.pads.input.caps.framerate
-      time = Membrane.Time.second() * denom / num
-      {{:ok, buffer: {:output, buffer}}, timestamp + time}
-    end
-  end
-
   test "video" do
     children = [
       file: %Membrane.Element.File.Source{location: "test/fixtures/in_video.h264"},
-      parser: %Membrane.Element.FFmpeg.H264.Parser{alignment: :nal, framerate: {30, 1}},
-      timestamper: Timestamper,
+      parser: %Membrane.Element.FFmpeg.H264.Parser{framerate: {30, 1}, attach_nalus?: true},
       payloader: Membrane.MP4.Payloader.H264,
       cmaf: Membrane.MP4.CMAF.Muxer,
       sink: Membrane.Testing.Sink
@@ -88,7 +62,7 @@ defmodule Membrane.MP4.IntegrationTest do
 
   defp assert_mp4_equal(output, ref_file) do
     ref_output = File.read!(Path.join("test/fixtures", ref_file))
-    assert Container.parse!(output) == Container.parse!(ref_output)
-    assert output == ref_output
+    assert {ref_file, Container.parse!(output)} == {ref_file, Container.parse!(ref_output)}
+    assert {ref_file, output} == {ref_file, ref_output}
   end
 end
