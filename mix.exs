1b24b1c8c306c5597858073dd6ca9446985542c9
Przemyslaw Roznawski <roznawski.przemyslaw@gmail.com>
2022-09-19 13:52:41 +0200

    version_0.16.2

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Membrane.MP4.Plugin.MixProject do
   use Mix.Project
 
-  @version "0.16.1"
+  @version "0.16.2"
   @github_url "https://github.com/membraneframework/membrane_mp4_plugin"
 
   def project do
