836c36604847d141b4e1b5617ce24106c75d44d0
Łukasz Kita <lukasz.kita0@gmail.com>
2023-05-26 10:49:01 +0200

    Release v0.22.0 (#63)

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Membrane.MP4.Plugin.MixProject do
   use Mix.Project
 
-  @version "0.21.0"
+  @version "0.22.0"
   @github_url "https://github.com/membraneframework/membrane_mp4_plugin"
 
   def project do
