f37ea37a4bcc62e59187bb44c26112079d37fa1a
Jakub Perżyło <jakub.perzylo@swmansion.com>
2023-08-10 16:30:35 +0200

    Omit stream format generation on PPS change in h264 payloader (#80)

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Membrane.MP4.Plugin.MixProject do
   use Mix.Project
 
-  @version "0.26.1"
+  @version "0.27.0"
   @github_url "https://github.com/membraneframework/membrane_mp4_plugin"
 
   def project do
