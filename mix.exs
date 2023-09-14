a3379da6f4a49dc08d2895ef30d9b3e13c72a9fd
feliks.pobiedzinski@swmansion.com <feliks.pobiedzinski@swmansion.com>
2023-09-14 16:29:53 +0200

    Fix deps

--- mix.exs
+++ mix.exs
@@ -38,7 +38,7 @@ defmodule Membrane.MP4.Plugin.MixProject do
   defp deps do
     [
       # {:membrane_core, "~> 0.12.3"},
-      {:membrane_core, path: "../membrane_core", override: true},
+      {:membrane_core, github: "membraneframework/membrane_core", branch: "wip", override: true},
       {:membrane_mp4_format, "~> 0.8.0"},
       {:membrane_cmaf_format, "~> 0.7.0"},
       {:membrane_aac_format, "~> 0.8.0"},
