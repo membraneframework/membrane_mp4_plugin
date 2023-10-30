718183f950a5b1ca9fea9b6df39528b8bc1f2081
Łukasz Kita <lukasz.kita0@gmail.com>
2023-10-30 10:58:35 +0100

    Bump to v0.30.2 (#97)

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Membrane.MP4.Plugin.MixProject do
   use Mix.Project
 
-  @version "0.30.1"
+  @version "0.30.2"
   @github_url "https://github.com/membraneframework/membrane_mp4_plugin"
 
   def project do
