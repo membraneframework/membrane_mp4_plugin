2a10504dd14e70782c8e5772b7480f8c7d734391
Łukasz Kita <lukasz.kita0@gmail.com>
2025-01-20 15:49:22 +0100

    Implement reviewers suggestions

--- lib/membrane_mp4/demuxer/cmaf/samples_info.ex
+++ lib/membrane_mp4/demuxer/cmaf/samples_info.ex
@@ -37,44 +37,57 @@ defmodule Membrane.MP4.Demuxer.CMAF.SamplesInfo do
     boxes
     |> Enum.filter(fn {type, _content} -> type == :traf end)
     |> Enum.map(fn {:traf, box} -> box end)
-    |> Enum.flat_map(&handle_track/1)
+    |> Enum.flat_map(&handle_traf/1)
     |> Enum.sort_by(& &1.offset)
   end
 
-  defp handle_track(traf_box) do
-    track_id = traf_box.children[:tfhd].fields.track_id
+  defp handle_traf(traf_box) do
+    track_description = %{
+      track_id: traf_box.children[:tfhd].fields.track_id,
+      base_data_offset: traf_box.children[:tfhd].fields[:base_data_offset] || 0,
+      default_sample_duration: traf_box.children[:tfhd].fields[:default_sample_duration] || 0,
+      default_sample_size: traf_box.children[:tfhd].fields[:default_sample_size],
+      base_media_decode_time: traf_box.children[:tfdt].fields.base_media_decode_time || 0
+    }
 
-    base_data_offset = traf_box.children[:tfhd].fields[:base_data_offset] || 0
+    {samples, _ts_acc} =
+      Enum.filter(traf_box.children, fn {box_name, _box} -> box_name == :trun end)
+      |> Enum.flat_map_reduce(track_description.base_media_decode_time, fn {:trun, trun_box},
+                                                                           ts_acc ->
+        {samples, {_size_acc, new_ts_acc}} =
+          handle_trun(
+            trun_box,
+            ts_acc,
+            track_description
+          )
 
-    default_sample_duration = traf_box.children[:tfhd].fields[:default_sample_duration] || 0
+        {samples, new_ts_acc}
+      end)
 
-    default_sample_size = traf_box.children[:tfhd].fields[:default_sample_size]
+    samples
+  end
 
-    base_media_decode_time = traf_box.children[:tfdt].fields.base_media_decode_time || 0
+  defp handle_trun(
+         trun_box,
+         ts_acc,
+         track_description
+       ) do
+    Enum.map_reduce(
+      trun_box.fields.samples,
+      {track_description.base_data_offset + trun_box.fields.data_offset, ts_acc},
+      fn sample, {size_acc, ts_acc} ->
+        size = sample[:sample_size] || track_description.default_sample_size
+        duration = sample[:sample_duration] || track_description.default_sample_duration
 
-    Enum.filter(traf_box.children, fn {box_name, _box} -> box_name == :trun end)
-    |> Enum.flat_map_reduce(base_media_decode_time, fn {:trun, trun_box}, ts_acc ->
-      {samples, {_size_acc, ts_acc}} =
-        Enum.map_reduce(
-          trun_box.fields.samples,
-          {base_data_offset + trun_box.fields.data_offset, ts_acc},
-          fn sample, {size_acc, ts_acc} ->
-            size = sample[:sample_size] || default_sample_size
-            duration = sample[:sample_duration] || default_sample_duration
-
-            {%{
-               duration: duration,
-               ts: ts_acc,
-               size: size,
-               composition_offset: sample[:composition_offset] || 0,
-               offset: size_acc,
-               track_id: track_id
-             }, {size_acc + size, ts_acc + duration}}
-          end
-        )
-
-      {samples, ts_acc}
-    end)
-    |> elem(0)
+        {%{
+           duration: duration,
+           ts: ts_acc,
+           size: size,
+           composition_offset: sample[:composition_offset] || 0,
+           offset: size_acc,
+           track_id: track_description.track_id
+         }, {size_acc + size, ts_acc + duration}}
+      end
+    )
   end
 end
