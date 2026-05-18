a29ef64a88340fc56e5cf9f2bab4eea22a908bda
Łukasz Kita <lukasz.kita0@gmail.com>
2026-05-18 11:36:00 +0200

    Send Discontinuity event when using `start_at` (#124)
    * Send discontinuity if start_at != 0
    
    * Send discontinuity for each pad

--- lib/membrane_mp4/demuxer/demuxing_source.ex
+++ lib/membrane_mp4/demuxer/demuxing_source.ex
@@ -81,7 +81,8 @@ defmodule Membrane.MP4.Demuxer.DemuxingSource do
       Map.from_struct(opts)
       |> Map.merge(%{
         engine: nil,
-        pad_to_track_id: %{}
+        pad_to_track_id: %{},
+        should_send_discontinuity: %{}
       })
 
     {[], state}
@@ -145,7 +146,17 @@ defmodule Membrane.MP4.Demuxer.DemuxingSource do
             []
           end
 
-        {maybe_send_stream_format ++ [buffer: {pad, buffer}, redemand: pad], state}
+        maybe_send_discontinuity =
+          if state.should_send_discontinuity[pad] do
+            [event: {pad, %Membrane.Event.Discontinuity{duration: buffer.pts}}]
+          else
+            []
+          end
+
+        state = put_in(state.should_send_discontinuity[pad], false)
+
+        {maybe_send_stream_format ++
+           maybe_send_discontinuity ++ [buffer: {pad, buffer}, redemand: pad], state}
     end
   end
 
@@ -171,6 +182,7 @@ defmodule Membrane.MP4.Demuxer.DemuxingSource do
       end
 
     state = put_in(state.pad_to_track_id[pad], track_id)
+    state = put_in(state.should_send_discontinuity[pad], state.start_at != 0)
     {[], state}
   end
 
