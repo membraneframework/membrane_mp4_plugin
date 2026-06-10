5d164f90364e61ad6a492beef93fff3a26e65a56
Łukasz Kita <lukasz.kita0@gmail.com>
2026-06-10 10:27:41 +0200

    Update lib/membrane_mp4/container/parse_helper.ex
    Co-authored-by: Mateusz Front <mateusz.front@swmansion.com>

--- lib/membrane_mp4/container/parse_helper.ex
+++ lib/membrane_mp4/container/parse_helper.ex
@@ -31,7 +31,7 @@ defmodule Membrane.MP4.Container.ParseHelper do
       box = %{fields: fields, children: children, size: content_size, header_size: header_size}
       parse_boxes(data, schema, context, [{name, box} | acc])
     else
-      header_content: {:error, {:unknown_box_name, name, header_size, content_size}} ->
+      header: {:error, {:unknown_box_name, name, header_size, content_size}} ->
         case data do
           <<_header::binary-size(header_size), content::binary-size(content_size),
             remaining::binary>> ->
@@ -42,7 +42,7 @@ defmodule Membrane.MP4.Container.ParseHelper do
             {:ok, Enum.reverse(acc), data, context}
         end
 
-      header_content: _error ->
+      header: _error ->
         # more data needed
         {:ok, Enum.reverse(acc), data, context}
 
