35a0e8ea9c5ddfe33cb49610d0694708c1eba9d3
maslowalex <maslo.rails@gmail.com>
2026-08-17 15:59:18 +0300

    chore: bump membrane_av1_format to 0.3.1

--- mix.exs
+++ mix.exs
@@ -45,7 +45,7 @@ defmodule Membrane.MP4.Plugin.MixProject do
       {:membrane_h264_format, "~> 0.6.1"},
       {:membrane_h265_format, "~> 0.2.0"},
       {:membrane_opus_format, "~> 0.3.0"},
-      {:membrane_av1_format, "~> 0.3.0"},
+      {:membrane_av1_format, "~> 0.3.1"},
       {:membrane_file_plugin, "~> 0.17.0"},
       {:membrane_timestamp_queue, "~> 0.2.1"},
       {:bunch, "~> 1.5"},
